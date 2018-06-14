d1b96e19f247128677edccd90ed88c38dd54f2d1
Rosa Tu <rosalina.tu@gmail.com>
2018-06-14 18:09:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -13,7 +13,6 @@ var playlist = new Object({ Cher: 'Do You Believe In Love After Love'})
 
 function removeFromPlaylist (Object, artist) {
 
-delete Object.artist = true
+delete Object.artist 
 
-return Object 
 }
