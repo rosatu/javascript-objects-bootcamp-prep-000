ae30980e28311a4463f8c4d36b5d2bd5593980da
Rosa Tu <rosalina.tu@gmail.com>
2018-06-14 17:42:28 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = new Object({ Cher: 'Do You Believe In Love After Love'})
 
 function updatePlaylist(obj, name, title ) 
 {
- playlist.Cher = [name: 'title']
+ playlist.Cher = [name, 'title']
  
  return playlist
 }
