6a2ceded40896bfe3c27b96a576e12614ddf72a7
Rosa Tu <rosalina.tu@gmail.com>
2018-06-14 18:07:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,6 +8,15 @@ obj[artist] = song
  return obj
 }
 
+var playlist = new Object({ Cher: 'Do You Believe In Love After Love'})
+ 
+// the `delete` operator returns `true` if it has successfully
+// deleted, `false` otherwise
+delete Object.artist
+
+return Object 
+
+
 function removeFromPlaylist (playlist, artist) 
 {
  {delete playlist.artist
