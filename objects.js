91c6e90c18f14d1997bb038292d47b7592578e44
Rosa Tu <rosalina.tu@gmail.com>
2018-06-14 17:28:28 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = new Object()
 
 var playlist = new Object({ Sia: 'I\'m alive'})
 
-function updatePlaylist(obj, name, title ) 
+function updatePlaylist([playlist], name, title ) 
 {
  return Object.assign({}, obj, {[name]:title})
 }
